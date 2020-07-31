d3777632ea1896814f57238abb27139f7f3ba85a
Keflem trindade <keflemtn@gmail.com>
2020-07-31 16:30:46 -0400

    tela feita

--- lib/home.dart
+++ lib/home.dart
@@ -6,11 +6,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _preco = "0";
+
+  void _recuperarPreco() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Preço do Bitcoin"),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("img/bitcoin.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$:" + _preco,
+                  style: TextStyle(fontSize: 35),
+                ),
+              ),
+              RaisedButton(
+                  child: Text(
+                    "Atualizar",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  color: Colors.orange,
+                  padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                  onPressed: _recuperarPreco),
+            ],
+          ),
+        ),
       ),
     );
   }
